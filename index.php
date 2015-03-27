a7101d1a61fb60c7fcfdd537feaa57984313c837
rjalgeo <rjalgeo@gmail.com>
2015-03-27 11:12:11 -0500

    Git TEST
    Testing Git Commit

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 			<?php if (have_posts () ) : while (have_posts ()) : the_post(); 
 			?>
 			<section>
+				<!-- GIT TEST! -->
 				<a href="<?php the_permalink(); ?>"><h1><?php the_title_attribute(); ?></h1></a>
 				<?php the_content();?>
 				<!--<p>This Life Clinic Blog will highlight the most cutting edge information and tutorials in the physical therapy industry!</p> -->
